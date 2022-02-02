f2007ec7f77667e4d5d81c7e39e8ee193ed0e99b
OskarsZageris <oskars.zageris@gmail.com>
2022-02-02 11:25:39 +0200

    Done

--- 05-array/07.php
+++ 05-array/07.php
@@ -27,28 +27,75 @@
 //referenceToCoco.HasSameMotherAs(referenceToRocky). Show that the new method works in the DogTest main method.
 
 
-class Dog {
+class Dog
+{
     private string $name;
     private string $sex;
+    private string $mother;
+    private string $father;
 
-public function __construct($name,$sex)
+    public function __construct(string $name, string $sex, string $mother = "Unknown", string $father = "Unknown")
+    {
+        $this->name = $name;
+        $this->sex = $sex;
+        $this->mother = $mother;
+        $this->father = $father;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getSex()
+    {
+        return $this->sex;
+    }
+
+    public function getMother()
+    {
+        return $this->mother;
+    }
+
+    public function getFather()
+    {
+        return $this->father;
+    }
+
+    public function hasSameMotherAs(Dog $dog): string
+    {
+        if ($this->mother === $dog->getMother()) {
+            return "True";
+        } else return "False";
+    }
+}
+
+class DogTest
 {
-    $this->name=$name;
-    $this->sex=$sex;
+    private array $dogs;
+
+    public function __construct(array $dogs)
+    {
+        $this->dogs = $dogs;
+    }
+
+    public function getDogs()
+    {
+        return $this->dogs;
+    }
+
 }
-}
-$max=new Dog("Max","male");
-$rocky=new Dog("Rocky","male");
-$sparky=new Dog("Sparky","male");
-$buster=new Dog("Buster","male");
-$sam=new Dog("Sam","male");
-$lady=new Dog("Lady","female");
-$molly=new Dog("Molly","female");
-$coco=new Dog("Coco","female");
-class DogTest{
-    private array $dogs=[];
-public function __construct(Dog $name)
-{
-    $this->dogs[]=$name;
-}
-}
+
+$dogs = new DogTest([
+    $max = new Dog("Max", "male", "Lady", "Rocky"),
+    $rocky = new Dog("Rocky", "male", "Molly", "Sam"),
+    $sparky = new Dog("Sparky", "male"),
+    $buster = new Dog("Buster", "male", "Lady", "Sparky"),
+    $sam = new Dog("Sam", "male"),
+    $lady = new Dog("Lady", "female"),
+    $molly = new Dog("Molly", "female"),
+    $coco = new Dog("Coco", "female", "Molly", "Sparky"),
+]);
+
+echo $coco->getName()." has same mother as ".$rocky->getName().": ".$coco->hasSameMotherAs($rocky).PHP_EOL;
+echo $buster->getName()." has same mother as ".$rocky->getName().": ".$buster->hasSameMotherAs($rocky).PHP_EOL;
